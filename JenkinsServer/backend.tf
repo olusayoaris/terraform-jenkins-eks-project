a805e727e5c296f9017d60d36745b632b29fe6ad
JendareyTechnologies <ak@jendareyconsulting.com>
2023-12-23 06:55:12 -0600

    Update backend.tf

--- JenkinsServer/backend.tf
+++ JenkinsServer/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformprojectbucket"
+    bucket = "jendareyprojectbucket"
     key    = "terraform/backend/jenkins-server/terraform.tfstate"
     region = "us-east-1"
   }
